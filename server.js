7a2d214636ee6be29af2d2bb45f2f1609ea2f00e
OksanaNova <o.nova0804@gmail.com>
2025-06-11 14:12:29 -0600

    reading and creating new file

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-READING
+// READING
+
+// const fs = require('fs');
+
+// const data = fs.readFile('index.html', function(err, data) {
+//     if (err) throw err;
+//     console.log(data.toString());
+// })
+
+// CREATE A NEW FILE       
 
 const fs = require('fs');
 
-const data = fs.readFile('index.html', function(err, data) {
-    if (err) throw err;
-    console.log(data.toString());
+fs.writeFile('newFile.html', 'This is my new file', function(err) {
+    if(err) throw err;
+    console.log('NEW FILE CREATED')
 })
