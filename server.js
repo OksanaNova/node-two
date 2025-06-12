e1fa5a7373e78a1f78369be69dfa22fc4f03c252
OksanaNova <o.nova0804@gmail.com>
2025-06-11 18:33:32 -0600

    append, delete

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // READING
 
-// const fs = require('fs');
+const fs = require('fs');
 
-// const data = fs.readFile('index.html', function(err, data) {
-//     if (err) throw err;
-//     console.log(data.toString());
-// })
+const data = fs.readFile('index.html', function(err, data) {
+    if (err) throw err;
+    console.log(data.toString());
+})
 
 // CREATE A NEW FILE       
 
@@ -14,4 +14,22 @@ const fs = require('fs');
 fs.writeFile('newFile.html', 'This is my new file', function(err) {
     if(err) throw err;
     console.log('NEW FILE CREATED')
+})
+
+// APPENDING NEW INFORMATION
+
+const fs = require('fs');
+
+fs.appendFile('newFile.html', 'New information', function(err) {
+    if (err) throw err;
+    console.log('FILE UPDATED')
+})
+
+// DELETING A FILE
+
+const fs = require('fs');
+
+fs.unlink('newFile.html', function(err) {
+    if (err) throw err;
+    console.log('FILE DELETED')
 })
